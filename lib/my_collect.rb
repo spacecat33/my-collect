9237170e29ef20cbdd7d12a7b39934d792206300
Janan Akkad <jananakkad@hotmail.co.uk>
2022-04-13 21:30:23 +0100

    Completed assignment

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+def my_collect(array)
+    i = 0
+    collection = []
+    while i < array.length do
+        collection << yield(array[i])
+        i += 1
+    end
+   collection
+end  
 
+#or 
 
+def my_collect(array)
+    i = 0 
+    collection = []
+    while i < array.length 
+        array.each do |language|
+        language.upcase
+    end
+        collection << yield(array[i])
+        i += 1
+    end 
+    collection
+end
